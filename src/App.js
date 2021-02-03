6b490d8a77b3cd4e1bbd0bd85717376dee65a9ea
jamie-ctm <jamie.paterson@bglgroup.co.uk>
2021-02-03 15:36:52 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,50 @@
-import React from 'react';
-// import Socket from 'socket';
-import { io } from "socket.io-client";
+import logo from './logo.svg';
+import './App.css';
 
-/*
-const io = require('socket.io')(80);
-const cfg = require('./config.json');
-const tw = require('node-tweet-stream')(cfg);
+// import { io } from 'socket.io-client';
 
-*/
-function App() {    
-    const socket = io("https://socketio-chatroom.herokuapp.com");
-        socket.on('connect', function() { 
-            console.log('connected');
+var io = require('socket.io-client');
+const socket = io("http://localhost:3001");
 
-            socket.emit('joinRoom', {
-                username: 'Bob',
-                room: 'react-coding'
-            }, function (data) {
+function App() {
 
-                socket.emit('message', {
-                username: 'Bob',
-                text: 'Your message goes here'
-            });
+  console.log("boo");
+ 
+  socket.on('connect', function () {
+    console.log('FINALLY')
+      socket.emit('joinRoom', {
+          username: 'Bob',
+          room: 'react-coding'
+      }, function (data) {
+          socket.emit('message',
+              {
+                  username: 'Bob',
+                  text: 'Your message goes here'
+              });
+      });
+  });
 
-
-            });
-
-        });
-
-    return (
-        <div className="App">
-
-        </div>
-    );
-
+  socket.on('message', function (message) {
+    console.log('BOB received a message', message);
+  });
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 
 export default App;
