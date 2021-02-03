b817cc144e29a60d72caa4116f96bc02ad4be99d
jamie-ctm <jamie.paterson@bglgroup.co.uk>
2021-02-03 16:10:02 +0000

    Removed breaking changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
 
 var io = require('socket.io-client');
@@ -28,20 +29,7 @@ function App() {
   });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>hi</h1>
     </div>
   );
 }
