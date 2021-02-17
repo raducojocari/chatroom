6058379dd6370843b706e110ba6a46710664c2a8
rc <cojocari.radu.stelian@gmail.com>
2021-02-17 16:31:12 +0000

    Conver Login component to TS

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,13 +4,13 @@ import { loginUser } from '../actions';
 
 import './Login.css';
 
-const Login = (props) => {
+const Login = (props: any) => {
   console.log('props', props);
 
-  const [username, setUserName] = useState();
+  const [username, setUserName] = useState('');
   const dispatch = useDispatch();
 
-  const handleChange = (e) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserName(e.target.value);
   };
 
@@ -18,15 +18,16 @@ const Login = (props) => {
     const roomAreas = document.getElementsByClassName('nav_button');
     setTimeout(() => {
       for (let i = 0; i < roomAreas.length; i++) {
-        roomAreas[i].click();
+        const currentRoom = roomAreas[i] as HTMLElement;
+        currentRoom.click();
       }
     }, 100);
     setTimeout(() => {
-      document.getElementById('nav_button').click();
+      document.getElementById('nav_button')?.click();
     }, 100);
   };
 
-  const handleSubmitLogin = (e) => {
+  const handleSubmitLogin = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log('handleSubmitLogin:', username);
     dispatch(loginUser(username));
